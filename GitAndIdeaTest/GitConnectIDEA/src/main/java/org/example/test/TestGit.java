8543a2c7d9dcbd5d74b89d1d31542076912dc873
lhr <l13263131238@163.com>
2022-08-27 18:14:07 +0800

    third git

--- GitAndIdeaTest/GitConnectIDEA/src/main/java/org/example/test/TestGit.java
+++ GitAndIdeaTest/GitConnectIDEA/src/main/java/org/example/test/TestGit.java
@@ -3,5 +3,6 @@ package org.example.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("这是第一次修改文件");
+        System.out.println("这是第二次修改文件");
     }
 }
