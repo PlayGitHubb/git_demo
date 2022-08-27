82e8ac0e0163a52f2debd80e2810ac262d111acb
lhr <l13263131238@163.com>
2022-08-27 19:16:21 +0800

    分支合并 git

--- GitAndIdeaTest/GitConnectIDEA/src/main/java/org/example/test/TestGit.java
+++ GitAndIdeaTest/GitConnectIDEA/src/main/java/org/example/test/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("这是第一次修改文件");
         System.out.println("这是第二次修改文件");
+        System.out.println("这是分支文件");
     }
 }
